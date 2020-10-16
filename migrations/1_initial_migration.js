a18413ac83a88631c48e902be96479ccb0a79319
bobeu <bobmatea27@gmail.com>
2020-10-16 06:13:29 +0100

    Migration error fixed

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,4 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
-
+var IPFSInbox = artifacts.require("IPFSInbox.sol");
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(IPFSInbox);
 };
